6491a86b4db2bd2c979e33287d92c5032af7e5e5
LackupL58 <beekoop158+gh@gmail.com>
2025-12-14 01:57:41 -0700

    feat: use of audio plugin

--- scripts/mods/MuteImpacts/MuteImpacts.lua
+++ scripts/mods/MuteImpacts/MuteImpacts.lua
@@ -14,3 +14,8 @@ mod:info("v"..mod.version.." loaded uwu nya :3")
 -- ###################################################################
 -- MOD LOGIC
 -- ###################################################################
+local audio_plugin = get_mod("Audio")
+audio_plugin.silence_sounds({
+    "wwise/events/weapon/play_explosion_force_sml",
+    "wwise/events/weapon/play_hit_indicator_melee_slashing_super_armor_no_damage",
+})
