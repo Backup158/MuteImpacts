3f4fa5a17f0eea45ad822ea3b5e8f54e6daf5f8b
LackupL58 <beekoop158+gh@gmail.com>
2025-12-14 03:07:17 -0700

    feat: more mute sounds

--- MuteImpacts/scripts/mods/MuteImpacts/SoundsToMute.lua
+++ MuteImpacts/scripts/mods/MuteImpacts/SoundsToMute.lua
@@ -4,9 +4,12 @@ mod.sounds_to_toggle = {
 	forcestaff_primary_fire = "wwise/events/weapon/play_explosion_force_sml",
 	super_armor_no_damage_melee_slashing = "wwise/events/weapon/play_hit_indicator_melee_slashing_super_armor_no_damage",
     super_armor_no_damage_melee = "wwise/events/weapon/play_hit_indicator_melee_super_armor_no_damage",
+    melee_hits_blunt_no_damage = "wwise/events/weapon/play_melee_hits_blunt_no_damage",
+    melee_hits_sword_no_damage = "wwise/events/weapon/play_melee_hits_sword_no_damage",
+    melee_hits_axe_armor = "wwise/events/weapon/play_melee_hits_axe_armor",
+    melee_hits_blunt_armor = "wwise/events/weapon/play_melee_hits_blunt_armor",
+    melee_hits_sword_armor = "wwise/events/weapon/play_melee_hits_sword_armor",
+    melee_hits_knife_armor = "wwise/events/weapon/play_melee_hits_knife_armor",
+    bullet_hits_gen_damage_negated = "wwise/events/weapon/play_bullet_hits_gen_damage_negated",
+    bullet_hits_laser_damage_negated = "wwise/events/weapon/play_bullet_hits_laser_damage_negated",
 }
--- check out:
---  melee_hits_blunt_no_damage
---  melee_hits_sword_no_damage
---  play_bullet_hits_gen_damage_negated
---  play_bullet_hits_laser_damage_negated
