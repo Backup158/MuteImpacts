6da9a3280150d388303c2d39735ed1e4938c6f79
LackupL58 <beekoop158+gh@gmail.com>
2025-12-14 02:30:33 -0700

    style: comment segregation

--- MuteImpacts/scripts/mods/MuteImpacts/MuteImpacts_data.lua
+++ MuteImpacts/scripts/mods/MuteImpacts/MuteImpacts_data.lua
@@ -1,5 +1,8 @@
 local mod = get_mod("MuteImpacts")
 
+-- ################################
+-- Local References for Performance
+-- ################################
 local pairs = pairs
 local table = table
 local table_insert = table.insert
@@ -7,6 +10,9 @@ local table_insert = table.insert
 mod:io_dofile("MuteImpacts/scripts/mods/MuteImpacts/SoundsToMute")
 local sounds_to_toggle = mod.sounds_to_toggle
 
+-- ################################
+-- Widget Creation
+-- ################################
 local final_widgets = {}
 for setting_name, _ in pairs(sounds_to_toggle) do 
 	table_insert(final_widgets, {
