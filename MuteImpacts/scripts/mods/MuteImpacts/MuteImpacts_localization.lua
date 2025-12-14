4b2e91770fd0e83c89f2ec71c46781ee3fd4ccc7
LackupL58 <beekoop158+gh@gmail.com>
2025-12-14 02:40:35 -0700

    feat: overly complex automatic localization

--- MuteImpacts/scripts/mods/MuteImpacts/MuteImpacts_localization.lua
+++ MuteImpacts/scripts/mods/MuteImpacts/MuteImpacts_localization.lua
@@ -1,5 +1,31 @@
 local mod = get_mod("MuteImpacts")
 
+-- ################################
+-- Local References for Performance
+-- ################################
+local pairs = pairs
+
+-- ################################
+-- Localization
+-- ################################
+local localizations_to_reuse = {
+	armor_type = {
+		super_armor = {
+			en = "Carapace",
+		},
+	},
+	damage_done = {
+		no_damage = {
+			en = "No Damage",
+		},
+	},
+	damage_type = {
+		melee_slashing = {
+			en = "Melee Slashing",
+		},
+	},
+}
+
 local localizations = {
 	mod_name = {
 		en = "Mute Impacts",
@@ -14,9 +40,16 @@ local localizations = {
 	forcestaff_primary_fire = {
 		en = "Force Staff Primary Fire",
 	},
-	super_armor_no_damage_melee_slashing = {
-		en = "Carapace: No Damage (Melee Slashing)",
-	},
 }
+for armor_type, armor_type_localization in pairs(localizations_to_reuse.armor_types) do
+	for damage_done, damage_done_localization in pairs(localizations_to_reuse.damage_done) do
+		for damage_type, damage_type_localization in pairs(localizations_to_reuse.damage_type) do
+			localizations[armor_type.."_"..damage_done.."_"..damage_type] = {
+				-- super_armor_no_damage_melee_slashing = "Carapace: No Damage (Melee Slashing)"
+				en = armor_type_localization["en"]..": "..damage_done_localization["en"].." ("..damage_type_localization["en"]..")"
+			}
+		end 
+	end 
+end 
 
 return localizations
