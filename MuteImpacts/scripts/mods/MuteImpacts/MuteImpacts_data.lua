56382b511fb19472bed4d7375f0dfc679c09a89d
Backup158 <beekoop158@gmail.com>
2026-03-20 15:51:42 -0500

    feat: made some options not muted by default

--- MuteImpacts/scripts/mods/MuteImpacts/MuteImpacts_data.lua
+++ MuteImpacts/scripts/mods/MuteImpacts/MuteImpacts_data.lua
@@ -6,6 +6,7 @@ local mod = get_mod("MuteImpacts")
 local pairs = pairs
 local table = table
 local table_insert = table.insert
+local table_contains = table.contains
 
 mod:io_dofile("MuteImpacts/scripts/mods/MuteImpacts/SoundsToMute")
 local sounds_to_toggle = mod.sounds_to_toggle
@@ -14,11 +15,24 @@ local sounds_to_toggle = mod.sounds_to_toggle
 -- Widget Creation
 -- ################################
 local final_widgets = {}
+
+-- Manually tracking things to not disable here
+-- Because I'm too lazy to turn every entry in SoundsToMute into a subtable
+local sounds_to_not_disable_by_default = {
+	"melee_hits_light_axe",
+	"melee_hits_heavy_axe",
+	"ranged_hits_unarmored_gen",
+	"ranged_hits_unarmored_laser",
+}
+
 for setting_name, _ in pairs(sounds_to_toggle) do 
+	local truth = true
+	if table_contains(sounds_to_not_disable_by_default, setting_name) then truth = false end
+
 	table_insert(final_widgets, {
 		setting_id = setting_name,
         type = "checkbox",
-        default_value = true,
+        default_value = truth,
 	})
 end
 
